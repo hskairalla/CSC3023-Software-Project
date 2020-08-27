6b2789c139888e76b1f4a4a7cd3e8671a77b3e3d
hudsonherr <69992366+hudsonherr@users.noreply.github.com>
2020-08-26 19:35:14 -0700

    Add files via upload
    This is what I've been working on. Not done yet

--- PPApplication.java
+++ PPApplication.java
@@ -15,10 +15,36 @@ import java.net.*;
 import javafx.geometry.*;
 import javafx.animation.*;
 
+
+//trying to add image
+//import javafx.scene.image.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException; 
+import javafx.application.Application; 
+import javafx.scene.Group; 
+import javafx.scene.Scene; 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;  
+import javafx.stage.Stage; 
+
+//
+
+
+
+
+
 public class PPApplication extends Application
 {
-   public void start(Stage stage)
+   public void start(Stage stage) throws FileNotFoundException 
    {
+      
+      
+      
+      
+      
+      
+      
+      
       //Create flowpane root
       FlowPane root = new FlowPane();
       
@@ -27,11 +53,54 @@ public class PPApplication extends Application
       
       //add canvas to root flowpane
       root.getChildren().add(canvas);
+      
+      
+      
+      
+      
+      /*
+      //ADD MENU IMAGE
+      Image menuImage = new Image("https://www.myabandonware.com/media/screenshots/c/contraption-zack-1dc/contraption-zack_1.gif");
+      ImageView imageView = new ImageView(menuImage);
+      root.getChildren().add(imageView);
+      */
+      
+      
+      
+      Image image = new Image("https://www.myabandonware.com/media/screenshots/c/contraption-zack-1dc/contraption-zack_1.gif");
+      ImageView mv = new ImageView(image);
+      
+      //Group root = new Group();
+      root.getChildren().addAll(mv);
+
+
+
 
       //Set scene
-      Scene scene = new Scene(root, 800, 800);//sets up window 
+      Scene scene = new Scene(root, 500,500);//sets up window
       stage.setScene(scene);
       stage.setTitle("Puzzle Project Application");
+      
+      Button start = new Button("Start");
+      Button load = new Button("load");
+      
+      canvas.getChildren().add(start);
+      canvas.getChildren().add(load);
+      //
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      //
       stage.show();
    
       canvas.requestFocus();
