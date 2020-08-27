77b48aec937d8b67c7bb30edf9dcbd8e028ef236
hskairalla <62540171+hskairalla@users.noreply.github.com>
2020-08-26 20:16:32 -0700

    Update PPApplication.java

--- PPApplication.java
+++ PPApplication.java
@@ -14,26 +14,11 @@ import javafx.scene.image.*;
 import java.net.*;
 import javafx.geometry.*;
 import javafx.animation.*;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException; 
-import javafx.application.Application; 
-import javafx.scene.Group; 
-import javafx.scene.Scene; 
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;  
-import javafx.stage.Stage; 
-
-
-
-
-
-
 
 public class PPApplication extends Application
 {
-   public void start(Stage stage) throws FileNotFoundException 
+   public void start(Stage stage)
    {
-      
       //Create flowpane root
       FlowPane root = new FlowPane();
       
@@ -41,67 +26,12 @@ public class PPApplication extends Application
       PPCanvas canvas = new PPCanvas();
       
       //add canvas to root flowpane
-      //root.getChildren().add(canvas);
-      
-      
-      
-      
-      //ADD MENU IMAGE
-   
-      Image menuImage = new Image("https://www.myabandonware.com/media/screenshots/c/contraption-zack-1dc/contraption-zack_1.gif");
-      //ImageView imageView = new ImageView(menuImage);
-
-      BackgroundImage menuBackgroundImage = new BackgroundImage(menuImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
-      //root.setBackground(new Background(menuBackgroundImage));
-      
-      // create Background 
-      Background background = new Background(menuBackgroundImage); 
-  
-      // set background 
-      root.setBackground(background);
-      
-      
-      
-      
-      
-       
-      //Add menu buttons
-      Button start = new Button("Start");
-      Button load = new Button("load");
-      root.getChildren().add(start);
-      root.getChildren().add(load);      
-      
-      
-      
-      
-      
-      
-      
+      root.getChildren().add(canvas);
 
       //Set scene
-      Scene scene = new Scene(root, 500,500);//sets up window
+      Scene scene = new Scene(root, 800, 800);//sets up window 
       stage.setScene(scene);
       stage.setTitle("Puzzle Project Application");
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      //
       stage.show();
    
       canvas.requestFocus();
