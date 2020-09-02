6fde79d08587a0820fb78a74fc80605dfdafe38c
hskairalla <62540171+hskairalla@users.noreply.github.com>
2020-09-02 16:17:10 -0700

    Update PPApplication.java

--- PPApplication.java
+++ PPApplication.java
@@ -153,9 +153,30 @@ public class PPApplication extends Application
       {    
            
          //import images
-         //Image brick = new Image("1Brick.png");//20
-         Image uparrow = new Image("uparrow.png");//21 
-         Image background = new Image("background.jpg");
+
+         //Image background = new Image("background.jpg");
+         Image whiteTile = new Image("White_Tile.png");//03
+         Image blackTile = new Image("Black_Tile.png");//04
+         Image metalTile = new Image("Metal_Tile.png");//05
+         Image greyTile = new Image("Grey_Tile.png");//06
+         Image greyTile4 = new Image("Grey_Tile4.png");//07
+         Image springTile = new Image("springTile.png");//10
+         Image uparrow = new Image("uparrow.png");//21          
+         Image brick = new Image("1Brick.png");//20 
+         Image greybrick = new Image("greybrick.png");//20
+         Image button1 = new Image("button.jpg");//24
+         Image purpleBU = new Image("Purple_BU.png");//30
+         Image orangeBU = new Image("Orange_BU.png");//31
+         Image yellowBU = new Image("Yellow_BU.png");//32
+         Image blueBU = new Image("Blue_BU.png");//33
+         Image purpleSpike = new Image("purpleSpike.png");//40
+         Image orangeSpike = new Image("orangeSpike.png");//41
+         Image yellowSpike = new Image("yellowSpike.png");//42
+         Image blueSpike = new Image("blueSpike.png");//43
+         Image blueSpikeR = new Image("blueSpikeR.png");//44
+         Image blueHoleR = new Image("blueHoleR.png");//45
+         Image blueHole = new Image("blueHole.png");//46
+         Image yellowHole = new Image("yellowHole.png");//47
           
          for(int i=0; i<40; i++)
          {
@@ -174,31 +195,131 @@ public class PPApplication extends Application
                if(level.getData(i,j)==2)//if value is 2 draw grey square at i,j
                {
                   gc.setFill(Color.GREY);
-                  gc.fillRect(j*20.0, i*20.0, 20, 20);  
+                  gc.fillRect(j*20.0, i*20.0, 20, 20);                
                }
-               if(level.getData(i,j)==12)//if value is 2 draw light grey square at i,j
+               if(level.getData(i,j)==3)//if value is 03 draw white tile at i,j
+               {
+                  gc.drawImage(whiteTile,j*20,i*20,80,80);               
+               }
+               if(level.getData(i,j)==4)//if value is 04 draw black tile at i,j
+               {
+                  gc.drawImage(blackTile,j*20,i*20,80,80);               
+               }
+               if(level.getData(i,j)==5)//if value is 05 draw metal tile at i,j
+               {
+                  gc.drawImage(metalTile,j*20,i*20,80,80);               
+               }
+               if(level.getData(i,j)==6)//if value is 06 draw grey tile at i,j
+               {
+                  gc.drawImage(greyTile,j*20,i*20,80,80);               
+               }
+               if(level.getData(i,j)==7)//if value is 07 draw 4 grey tile at i,j
+               {
+                  gc.drawImage(greyTile4,j*20,i*20,160,160);               
+               }
+               if(level.getData(i,j)==8)//if value is 08 draw half metal tile at i,j
+               {
+                  gc.drawImage(metalTile,j*20,i*20,80,40);               
+               }
+               if(level.getData(i,j)==9)//if value is 09 draw half grey tile at i,j
+               {
+                  gc.drawImage(greyTile,j*20,i*20,40,80);
+               }
+               if(level.getData(i,j)==10)//if value is 10 draw spring tile at i,j
+               {
+                  gc.drawImage(springTile,j*20,i*20,80,80);
+               }
+               if(level.getData(i,j)==12)//if value is 12 draw light grey square at i,j
                {
                   gc.setFill(Color.LIGHTGREY);
                   gc.fillRect(j*20.0, i*20.0, 20, 20);  
                }
                if(level.getData(i,j)==20)//if value is 20 draw brick image at i,j
                {
-                  //gc.drawImage(brick,j*20,i*20,80,80);
-                  
+                  gc.drawImage(brick,j*20,i*20,80,80); 
                }
                if(level.getData(i,j)==21)//if value is 21 draw arrow image at i,j
                {
-                  gc.drawImage(uparrow,j*20,i*20,40,40);
+                  gc.drawImage(uparrow,j*20,i*20,40,60);
                   
                }
-               /*
-               if(level.getData(i,j)==22)//if value is 22 draw jukebox gif at i,j
+               if(level.getData(i,j)==23)//if value is 23 draw grey brick at i,j
                {
-                  gc.drawImage(jukebox,j*20,i*20,60,120);
+                  gc.drawImage(greybrick,j*20,i*20,40,40);
                   
                }
-               */
-   
+               if(level.getData(i,j)==24)//if value is 24 draw button at i,j
+               {
+                  gc.drawImage(button1,j*20,i*20,80,80);
+                  
+               }
+               if(level.getData(i,j)==30)//if value is 30 draw unpressed purple button at i,j
+               {
+                  gc.drawImage(purpleBU,j*20,i*20,80,80);
+                  
+               }
+               if(level.getData(i,j)==31)//if value is 31 draw unpressed orange button at i,j
+               {
+                  gc.drawImage(orangeBU,j*20,i*20,80,80);
+                  
+               }
+               if(level.getData(i,j)==32)//if value is 32 draw unpressed yellow button at i,j
+               {
+                  gc.drawImage(yellowBU,j*20,i*20,80,80);
+                  
+               }
+               if(level.getData(i,j)==33)//if value is 33 draw unpressed blue button at i,j
+               {
+                  gc.drawImage(blueBU,j*20,i*20,80,80);  
+               }
+               if(level.getData(i,j)==34)//if value is 34 draw stetched unpressed purple button at i,j
+               {
+                  gc.drawImage(purpleBU,j*20,i*20,80,120);   
+               }
+               if(level.getData(i,j)==35)//if value is 35 draw stetched unpressed orange button at i,j
+               {
+                  gc.drawImage(orangeBU,j*20,i*20,80,120); 
+               }
+               if(level.getData(i,j)==36)//if value is 36 draw stetched unpressed yellow button at i,j
+               {
+                  gc.drawImage(yellowBU,j*20,i*20,80,120);   
+               }
+               if(level.getData(i,j)==37)//if value is 37 draw stetched unpressed blue button at i,j
+               {
+                  gc.drawImage(blueBU,j*20,i*20,80,120); 
+               }   
+               if(level.getData(i,j)==40)//if value is 40 draw purple spikes at i,j
+               {
+                  gc.drawImage(purpleSpike,j*20,i*20,80,20); 
+               }
+               if(level.getData(i,j)==41)//if value is 41 draw orange spikes at i,j
+               {
+                  gc.drawImage(orangeSpike,j*20,i*20,80,20); 
+               }
+               if(level.getData(i,j)==42)//if value is 42 draw yellow spikes at i,j
+               {
+                  gc.drawImage(yellowSpike,j*20,i*20,80,20); 
+               }
+               if(level.getData(i,j)==43)//if value is 43 draw blue spikes at i,j
+               {
+                  gc.drawImage(blueSpike,j*20,i*20,80,20); 
+               }
+               if(level.getData(i,j)==44)//if value is 44 draw blue spike rotated at i,j
+               {
+                  gc.drawImage(blueSpikeR,j*20,i*20,20,80); 
+               }
+               if(level.getData(i,j)==45)//if value is 45 draw blue hole rotated at i,j
+               {
+                  gc.drawImage(blueHoleR,j*20,i*20,20,80); 
+               }
+               if(level.getData(i,j)==46)//if value is 46 draw blue hole at i,j
+               {
+                  gc.drawImage(blueHole,j*20,i*20,80,20); 
+               }
+               if(level.getData(i,j)==47)//if value is 47 yellow blue hole at i,j
+               {
+                  gc.drawImage(yellowHole,j*20,i*20,80,20); 
+               }
                
                gc.setFill(Color.YELLOW);
                gc.fillRect(x, y, 20, 20);
