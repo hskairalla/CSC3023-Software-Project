6c14f13fa3cb02c84b2641067bffcb4f38da83a7
hskairalla <62540171+hskairalla@users.noreply.github.com>
2020-09-21 12:54:29 -0700

    Update PuzzleProjectNewFormat.java

--- PuzzleProjectNewFormat.java
+++ PuzzleProjectNewFormat.java
@@ -1,3 +1,20 @@
+//Puzzle Project Application
+//Written by Harrison Kairalla, Jake Bell, and Hudson Herr
+import javafx.application.*;
+import javafx.event.*;
+import javafx.stage.*;
+import javafx.scene.canvas.*;
+import javafx.scene.paint.*;
+import javafx.scene.*;
+import javafx.scene.input.*;
+import javafx.scene.layout.*;
+import java.util.*;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
+import java.net.*;
+import javafx.geometry.*;
+import javafx.animation.*;
+import java.io.*;
 import javax.imageio.*;
 
 public class PuzzleProjectNewFormat extends Application
